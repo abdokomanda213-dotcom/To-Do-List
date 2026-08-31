9ed9c66e0817ce1025f86f723c74a6b540e7314a
abdokomanda213-dotcom <abdokomanda213@gmail.com>
2026-08-31 15:46:43 +0300

    new

---
+++ src/components/Form.jsx
@@ -0,0 +1,14 @@
+ const Form =()=>{
+    return(
+        <>
+        <h1>mama</h1>
+        <p>sasa</p>
+        <h1>dddddddddddd</h1>
+        <p>sasaaaaaaaaaaaaaa</p>
+        <h1>ghghghghghghghghghghghghg</h1>
+        </>
+    )
+ }
+
+
+ export default Form;
